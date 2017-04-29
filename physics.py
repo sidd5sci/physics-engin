667c5dfc6952f2140e34b3884544f307d0729348
sidd5sci <sidd5sci@gmail.com>
2017-04-29 20:12:49 +0530

    update logics
    djskgk

--- physics.py
+++ physics.py
@@ -1,8 +1,15 @@
 
 from vector import *
 
-# universal gravitational constant
-G = 6.67408e-11 # m3 kg-1 s-2
+'''
+==========================================
+    Gloable constants of Physics
+==========================================
+'''
+# universal gravitational constant 
+G = 6.673e-11 # unit  [N m2 kg-2]
+# Unit charge
+e = 8.85e-32 # unit
 # earth mass
 MassEearth = 6e24  # kg
 '''
@@ -17,18 +24,43 @@ class physics(object):
         # mass
         self.mass = 1.0 # kg
         # motion physics / newton physics
-        self.pos = ZERO_VERTEX
-        self.velocity = ZERO_VECTOR
-        self.acc = ZERO_VECTOR
-        self.force = ZERO_VECTOR
-        self.time = 0.0
-        self.dt = 0.15
+        self.pos      = vertex(0,0,0)
+        self.velocity = vector((0,0,0),(0,0,0))
+        self.acc      = vector((0,0,0),(0,0,0))
+        self.force    = vector((0,0,0),(0,0,0))
+        self.time     = 0.0
+        self.dt       = 0.15
         # material physics
         self.IR = 0.0
         self.rigidity = 0.0
         self.color = []
+    def assign_dt(self,dt):
+        # this dt controls the change of speed of display of the change in the object position
+        self.dt = dt
+    def applyForce1(self,mag,direction):
+        # create a new zero vector
+        temp = vector((0,0,0),(0,0,0))
+        # copying the direction vector to the temprary vector 
+        temp.x,temp.y,temp.z = direction       
+        # multiplying the vector to the magnitude
+        temp.mult(mag)
+        # copying the temp force to the force
+        self.force.add(temp)
+        # updating the acc
+        self.updateAcc()
+    def applyForce2(self,mag,direction):
+        # create a new zero vector
+        temp = vector((0,0,0),(0,0,0))
+        # copying the direction vector to the temprary vector 
+        temp.x,temp.y,temp.z = direction.x,direction.y,direction.z
+        # multiplying the vector to the magnitude
+        temp.mult(mag)
+        # copying the temp force to the force
+        self.force.add(temp)
+        # updating the acc
+        self.updateAcc()
     def applyForce(self,force):
-        # adding force to object
+        # adding force to object must be vector
         self.force.add(force)
         # dividing force by mass of object
         self.force.divide(self.mass)
@@ -36,31 +68,159 @@ class physics(object):
         # multipling the acc to delta time
         self.acc.mult(self.dt)
         self.velocity.add(self.acc)
+    def copyForce(self,force):
+        # adding force to object
+        self.force.add(force)
+        # updating the acc
+        self.updateAcc()
     def realTimeForces(self,earthMass,dist):
         gravitation = (G * self.mass * earthMass)/ (dist**2)
         gForce = vector(0,-1,0)
         gForce.mult(gravitation)
         self.applyForce(gForce)
     def applyG(self):
-        g = vector(0,-9.8,0)
-        self.applyAcc(g)
+        g = vector((0,0,0),(0,0,0))
+        g.assign((0,0,0),(0,9.8,0))
+        self.acc.add(g)
+        
+        # update the velocity
+        self.updateVelocity()
     def applyAcc(self,acc):
         self.acc.add(acc)
         # multipling the acc to delta time
+        # formula a = v/t
         self.acc.mult(self.dt)
-        self.velocity.add(self.acc)
-    def collision(self,_object_):
-        print ctype
-    def boundryCollision(self,bound):
-        print ctype
+        # update the velocity
+        self.updateVelocity()
+        #self.velocity.add(self.acc)
+    def bound2d(self,window = []):
+        # right
+        if self.pos.x >= window[0] :
+            
+            self.velocity.reverseDir('x')
+        #left    
+        if self.pos.x <= 0 :
+            
+            self.velocity.reverseDir('x')
+        #bottom    
+        if self.pos.y >= window[1] :
+            #self.pos.y = window[1]
+            self.velocity.reverseDir('y')
+        #top    
+        if self.pos.y <= 0 :
+            #sself.pos.y = 0
+            self.velocity.reverseDir('y')
+    def bound2d_swap(self,window = []):
+        # right
+        if self.pos.x >= window[0] :
+            
+            self.pos.x = 0+1
+        #left    
+        if self.pos.x <= 0 :
+            
+            self.pos.x = window[0]-1
+        #bottom    
+        if self.pos.y >= window[1] :
+            #self.pos.y = window[1]
+            self.pos.y = 0+1
+        #top    
+        if self.pos.y <= 0 :
+            #sself.pos.y = 0
+            self.pos.y = window[1]-1
+    def collision(self,window):
+        # right
+        if self.pos.x >= window[0] :
+            self.pos.x = window[0]
+            temp = vector((0,0,0),(0,0,0))
+            temp.copy(self.acc)
+            temp.mult(self.mass)
+            direction = vector((0,0,0),(0,0,0))
+            direction.copy(self.velocity)
+            direction.normalized()
+            direction.reverseDir('x')          
+            self.applyForce2(temp.magCal(),direction)
+        #left    
+        if self.pos.x < 0 :
+            self.pos.x = 0
+            temp = vector((0,0,0),(0,0,0))
+            temp.copy(self.acc)
+            temp.mult(self.mass)
+            direction = vector((0,0,0),(0,0,0))
+            direction.copy(self.velocity)
+            direction.normalized()
+            direction.reverseDir('x')                
+            self.applyForce2(temp.magCal(),direction)
+        #bottom    
+        if self.pos.y >= window[1] :
+            self.pos.y = window[1]
+            temp = vector((0,0,0),(0,0,0))
+            temp.copy(self.acc)
+            temp.mult(self.mass)
+            direction = vector((0,0,0),(0,0,0))
+            direction.copy(self.velocity)
+            direction.normalized()
+            direction.reverseDir('y')                
+            self.applyForce2(temp.magCal(),direction)
+        #top    
+        if self.pos.y < 0 :
+            self.pos.y = 0
+            temp = vector((0,0,0),(0,0,0))
+            temp.copy(self.acc)
+            temp.mult(self.mass)
+            direction = vector((0,0,0),(0,0,0))
+            direction.copy(self.velocity)
+            direction.normalized()
+            direction.reverseDir('y')                
+            self.applyForce2(temp.magCal(),direction)
+    def updateAcc(self):
+        # formula a = f/m
+        # create a temp vector
+        temp = vector((0,0,0),(0,0,0))
+        # copying force in the temp
+        temp.copy(self.force)
+        # dividing force by mass of object
+        temp.divide(self.mass)
+        # copy the instatneous acc 
+        self.acc.add(temp)
+        # update the velocity
+        self.updateVelocity()
+    def updateVelocity(self):
+        # formula v = u + (a* deltaT)
+        # create a temp variable
+        temp = vector((0,0,0),(0,0,0))
+        # copy the acc to the temp
+        temp.copy(self.acc)
+        # multipling the acc to delta time
+        temp.mult(self.dt)
+        # updateing the velocity
+        self.velocity.add(temp)
+        # update the time
+        self.time += self.dt
+        
     def updatePos(self):
 
         if self.velocity.x != 0 or self.velocity.y != 0 or self.velocity.z != 0:
-            self.pos[0] += self.velocity.x*self.dt
-            self.pos[1] += self.velocity.y*self.dt
-            self.pos[2] += self.velocity.z*self.dt
+            self.pos.x += self.velocity.x*self.dt
+            self.pos.y += self.velocity.y*self.dt
+            self.pos.z += self.velocity.z*self.dt
 
+'''
+================================================================================
+================================================================================
+'''
+def isSamePos(pos1,pos2):
+    if pos1.x == pos2.x:
+        if pos1.y == pos2.y:
+            if pos1.z == pos2.z:
+                return True
+            else:
+                return False
+        else: return False
+    else:
+        return False
 
+def dist(p1,p2):
+    return math.sqrt(((p2.x-p1.x)**2) +((p2.y-p1.y)**2)+((p2.z-p1.z)**2))
 
 '''
 ==========================================
@@ -105,6 +265,10 @@ def IsCollied(rect1,rect2):
         return True
     else:
         return False
-
-
+# collision of two circular objects
+def isInsideCir(pos1,r1,pos2,r2):
+    if dist(pos1,pos2) > r1+r2:
+        return False
+    if dist(pos1,pos2) <= r1+r2:
+        return True
 #print IsCollied(surface1,surface2)
